7ff5dd8fde7aecb24b09e1b66b76fd3d27b696e4
VladimirMoroz95 <vladimirmoroz95@gmail.com>
2019-03-01 11:53:07 +0300

    add mocha and supertest

---
+++ test/users.test.js
@@ -0,0 +1,16 @@
+import supertest from 'supertest'
+import users from '../routes/users';
+
+
+const testServer = supertest(users);
+
+describe('test users api', function() {
+
+  it('should return users list', (done) => {
+    testServer
+      .get('/users')
+      .expect(200)
+      .expect('Content-type', /application\/json/)
+      .end(done)
+  })
+});
